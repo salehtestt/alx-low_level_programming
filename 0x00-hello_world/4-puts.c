fed59acee2416a2cbfff74768dfb0f96fe0ae7d1
saleh <saleh@saleh>
2023-03-16 13:01:46 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A program uses puts gunc
  *
  * Return Always 0 (succes)
  */
 int main (void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	 
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
-
