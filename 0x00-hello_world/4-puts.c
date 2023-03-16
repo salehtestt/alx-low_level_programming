5e9a0f4f7d1d54f1dea530de57fc9222e83613f5
saleh <saleh@saleh>
2023-03-16 14:52:34 -0700

    j

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - puts func
  * Return : Always 0 {success}
  */
-int main(void)
+int main()
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }	
