300a0c30b7d9c8ca37b083f43d92eb11dc1ab063
saleh <saleh@saleh>
2023-03-17 09:55:24 -0700

    hello

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/*more headers goes there*/
+/*betty style doc for function main goes there*/
+/**
+*main - hello.c
+*Return: Always 0 (succes)
+*/
+int main(void)
+{
+for (char n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+return (0);
+}
