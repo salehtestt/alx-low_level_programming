9e2fe2ac80dafbc62abc15fbd498a3e41474247c
saleh <saleh@saleh>
2023-03-17 05:10:23 -0700

    hello

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*main - Sizeof func
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("ize of a char:%lu byet(s)\n ", sizeof(char));
+printf("Size of a int:%lu byet(s)\n", sizeof(int));
+printf("Size of a long int: %lu byet(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byet(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byet(s)\n", sizeof(float));
+eturn (0);
+}
