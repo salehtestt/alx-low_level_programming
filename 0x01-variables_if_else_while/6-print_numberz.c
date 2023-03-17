5ad0ebba8e51424e570f295f32e1e41b50354731
saleh <saleh@saleh>
2023-03-17 12:10:01 -0700

    h

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdlib.h>
+/**
+*main - hello.c
+*Return: Always 0 (succes)
+*/
+int main(void)
+{
+char i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
