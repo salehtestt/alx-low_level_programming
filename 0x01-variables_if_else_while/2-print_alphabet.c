c22a8a3d8755dc89ae0ac04cfd72ec94bfa0e2d4
saleh <saleh@saleh>
2023-03-19 02:43:00 -0700

    h

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/*
+*main - Always print lowerr
+*Retyrn: A;ways 0 (success)
+*/
+int main(void)
+{
+int n = 97;
+for (n = 97; n <= 122; n++)
+{
+putchar(n);
+}
+putchar('\n'};
+return (0);
