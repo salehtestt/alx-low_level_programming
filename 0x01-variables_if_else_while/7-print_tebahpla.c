0bfc55da09099cec7415e41f9e6651689950b497
saleh <saleh@saleh>
2023-03-19 12:42:32 -0700

    ello

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+*main - hello.c
+*Return: Always 0 (succes)
+*/
+int main(void)
+{
+int n = 122;
+for (n = 122; n >= 97; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
