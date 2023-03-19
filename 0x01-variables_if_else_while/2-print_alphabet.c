a31ece88617c14228e8e98ff37660016516d3270
saleh <saleh@saleh>
2023-03-19 02:57:47 -0700

    j

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /*
-*main - Print lowerr case letters
+*main - prints the alphabet in lowercase, followed by a new line.
 *Retyrn: A;ways 0 (success)
 */
-int main(void)
+int main(void}
 {
 int n = 97;
 for (n = 97; n <= 122; n++)
