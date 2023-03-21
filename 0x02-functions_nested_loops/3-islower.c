9ed05b07edc0ffacc9ff2788c3033a2990bad7e2
saleh <saleh@example.com>
2023-03-21 15:57:26 -0700

    j

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,9 @@
 * Return: 1 if char is lowercase, otherwise 0.
 */
 int _islower(int c)
-{
-if (c == 97 && c <= 122)
-{
-return (1);
-}
-else
-{
-return (0);
-}
-}
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
+	}
