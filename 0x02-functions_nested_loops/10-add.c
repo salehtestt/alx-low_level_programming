51c2fcec5656fd261a1b10c2a9d9615ed3d671f9
saleh <saleh@example.com>
2023-03-22 05:29:19 -0700

    j

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - name of func
+ *@a: the first int
+ *@b: the soc int
+ *Return: return to the res
+ */
+int add(int a, int b)
+{
+int reslut = a + b;
+_putchar(reslut);
+return result;
+}
