e47c9d454b57778977a234c70864ccbcae76089c
saleh <saleh@example.com>
2023-03-22 04:30:30 -0700

    j

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *print_last_digit - func name prints 
- *@n: an int that i will print
- *Return: always to the last digit
- */
+*print_last_digit - func name prints
+*@n: an int that i will print
+*Return: always to the last digit
+*/
 int print_last_digit(int n)
 {
 int a;
@@ -11,16 +11,16 @@ a = n % 10;
 if (n > 0)
 {
 _putchar(a);
-return (n);
+return (a);
 }
-else if ( n == 0)
+else if (n == 0)
 {
 _putchar(a);
-return (n);
+return (a);
 }
 else
 {
-_putchar(a);
-return (-n);
+_putchar(-a);
+return (-a);
 }
 }
