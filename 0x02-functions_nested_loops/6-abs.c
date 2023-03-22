a8ab461a0ef0c581f52295d8732e012001816d07
saleh <saleh@example.com>
2023-03-22 03:55:56 -0700

    j

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs - print the absolute value of int
+ *@n: the int that i have to find the ans for
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+_putchar(n);
+}
+else if (n == 0)
+{
+_putchar(n);
+}
+else
+{
+_putchar(-n);
+}
+}
