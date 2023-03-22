14be5804d37287733b237cbe61c2ca527ca9ef1a
saleh <saleh@example.com>
2023-03-22 01:40:42 -0700

    j

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - print lower if it return to 1
+ *@c: int value shold be a lower alpha
+ *Return: 1 if it lower otherwise 0
+ */
+int _islower(int c)
+{
+if (c == 97; c <= 122; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
