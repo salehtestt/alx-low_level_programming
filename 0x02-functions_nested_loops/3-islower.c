c5a61e2a5116eb70341b84b60b014f6b1e96f966
saleh <saleh@example.com>
2023-03-22 11:58:02 -0700

    j

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_islower - the func name
+ *@c: an int that should be a lower case
+ *Return: 1 if it is lower and 0 if it is not
+ */
+int _islower(int c)
+{
+if (c == 97; && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
