62cafbf49cccf2bd9f3c15975886322a03b0d684
saleh <saleh@example.com>
2023-03-22 05:16:56 -0700

    j

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,32 +4,21 @@
 */
 void jack_bauer(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
+int i, j;
+i = 0;
+while (i < 24)
 {
-for (j = 0; j < 10; j++)
+j = 0;
+while (j < 60)
 {
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 _putchar('\n');
+j++;
+}
+i++;
 }
 }
-
