b190c379701620b54013eb3b42a9a6125ce16d8f
saleh <saleh@example.com>
2023-03-23 07:51:54 -0700

    j

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - is the func name
+ *@c: is an int 
+ *Return: 1 if its upper 0 if it is not
+ */
+int _isupper(int c)
+{
+if (c == 'A' && c == 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
