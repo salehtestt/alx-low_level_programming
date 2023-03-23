1f9cf7e507b93e6aa5c3cac29aead59d0b7451c7
saleh <saleh@example.com>
2023-03-23 08:17:55 -0700

    j

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *_isdigit -the func name
- *@c: an int
- *Return: 1 or 0
- */
+*_isdigit -the func name
+*@c: an int
+*Return: 1 or 0
+*/
 int _isdigit(int c)
 {
-if (c == '0' && c =='1' && c == '2' && c == '3' && c == '4' && c == '5' && c == '6' && c == '7' && c == '8' &&   c == '9')
+if (c == '0' && c == '1' && c == '2' && c == '3' && c == '4' && c == '5' && c == '6' && c == '7' && c == '8' &&   c == '9')
 {
-return (1)'
+return (1);
 }
 else
 {
