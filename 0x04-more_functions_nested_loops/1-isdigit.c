5a9c0c4aa0730d0993f37e13e5e4d72418cc8c6a
saleh <saleh@example.com>
2023-03-23 08:13:04 -0700

    j

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit -the func name
+ *@c: an int
+ *Return: 1 or 0
+ */
+int _isupper(int c)
+{
+if (c == '0' && c =='1' && c == '2' && c == '3' && c == '4' && c == '5' && c == '6' && c == '7' && c == '8' &&   c == '9')
+{
+return (1)'
+}
+else
+{
+return (0);
+}
+}
