fa14ca5a9e9358222489a1a5fc78b54d4d613bf4
saleh <saleh@example.com>
2023-03-24 07:57:32 -0700

    k

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_square(int size)
+{
+int a;
+int b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 1; a <= n; a++)
+{
+for (b = 1; b <= n; b++)
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
