add7e3f69d679f1713d8effa448fe8ade99c10bf
saleh <saleh@example.com>
2023-03-28 02:00:05 -0700

    j

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,14 +6,14 @@
 */
 void print_array(int *a, int n)
 {
-*a[n];
 int i;
-for (i = 1; i <= n; i++)
+for (i = 0; i < (n - 1); i++)
 {
-printf ("%d", *a);
-if (i < n)
-{
-printf (',');
-printf (' ');
+printf("%d, ", a[i]);
 }
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
+}
+printf("\n");
 }
