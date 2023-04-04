b3f8baf64ca99f2bb3a59e18644edcd547a9f9bb
saleh <saleh@example.com>
2023-04-04 04:55:13 -0700

    j

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
-*_strchr - the func name
-*@s: a char
-*@c: another char
+/**
+* _strchr - Entry point
+* @s: input
+* @c: input
+* Return: Always 0 (Success)
 */
 char *_strchr(char *s, char c)
 {
