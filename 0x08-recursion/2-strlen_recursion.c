70f0d50e7f4ca8eae6990b9b813ff23cf92a3106
saleh <saleh@example.com>
2023-04-05 04:30:44 -0700

    J

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strlen_recursion - the func name
+*@s: is the char
+*/
+int _strlen_recursion(char *s)
+{
+int i;
+i = 0;
+if (*s)
+{
+i++;
+i += _strlen_recursion(s + 1);
+}
+return (i);
+}
