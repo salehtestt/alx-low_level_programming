ad857512ead1d4de21c25246f89018c28f421a19
saleh <saleh@example.com>
2023-04-13 03:32:04 -0700

    L

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "maon.h"
+#include<stdlib.h>
+/**
+ * malloc_checked - the name of the func
+ * @b:is thee int
+ * exit: 98
+ * Rturn: m
+ */
+void *malloc_checked(unsigned int b)
+{
+int *m = malloc(b);
+if (m == 0)
+{
+exit(98);
+}
+return (m);
+}
